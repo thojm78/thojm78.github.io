7e4bee761a1835553de6ff13f2c3553dadfde7dc
Michelle Thao <thojmichele87@outlook.com>
2026-02-14 22:16:57 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,18 @@
 console.log('Hello')
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcome = document.querySelector('#welcome')
+if (isMorning) {
+    welcome.textContent = 'Good Morning'
+}
+else if (isAfternoon) {
+    welcome.textContent = 'Good Afternoon'
+}
+else if (isEvening) {
+    welcome.textContent = 'Good Evening'
+}
