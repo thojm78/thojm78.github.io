0dcf26e7fb8b81a40d49d9c2037c3ee7923315ff
Michelle Thao <thojmichele87@outlook.com>
2026-03-20 18:54:34 -0500

    Added carousel

--- site.js
+++ site.js
@@ -1,5 +1,6 @@
 console.log('Hello')
 
+
 // Assignment 3
 const hours = new Date().getHours() // get the current hour
 
@@ -18,5 +19,47 @@ else if (isEvening) {
     welcome.textContent = 'Good Evening'
 }
 
+
 // Assignment 4 - local storage
 localStorage.setItem("It's a secret to everybody.", 'Hi Ryan')
+
+
+// Assignment 5 - carousel 
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+showImages()
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)
+
+const prevButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+
+prevButton.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+
+
+nextButton.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
