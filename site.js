d8c22839741e52439889685e856610c7cb713b2c
Michelle Thao <thojmichele87@outlook.com>
2026-03-21 22:09:42 -0500

    Added API Fetch

--- site.js
+++ site.js
@@ -63,3 +63,76 @@ nextButton.addEventListener('click', () => {
     currentImage++
     showImages()
 })
+
+
+/*// Assignment 6 - to do list
+const todoList = document.querySelector('.todo-list')
+const todoInput = document.querySelector('#new-todo')
+const addButton = document.querySelector('#addTodo')
+
+// Get the list from local storage
+const todos = JSON.parse(localStorage.getItem('todo-list')) || []
+
+
+const renderTodos = () => {
+    // Clear the li's before we recreate them
+    todoList.innerHTML = ''
+
+    // Create and add new list items to the DOM
+    todos.forEach(todo => {
+        const li = document.createElement('li')
+        li.textContent = todo.text
+        todoList.append(li)
+    })
+}
+
+renderTodos()
+
+addButton.addEventListener('click', () => {
+    if (todoInput.value.trim() === '') {
+        return alert('Enter a todo item.')
+    }
+
+    // Add a new item to the list
+    todos.push({ text: todoInput.value, completed: false })
+
+    // Save the list to local storage
+    localStorage.setItem('todo-list', JSON.stringify(todos))
+
+    todoInput.value = ''
+
+    renderTodos()
+})*/
+
+
+// Assignment 7 - API fetch
+// arrow function to fetch random pokemon from PokeAPI
+const getRandomPokemon = async () => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random() * 150)
+
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// arrow function that takes pokemon object as parameter
+const renderPokemon = (pokemonObject) => {
+    const div = document.querySelector('.pokemonImage')
+    const img = document.createElement('img')
+    img.src = pokemonObject.sprites.front_default // url of the image from the 'front_default' property
+        img.alt = pokemonObject.name // name of the pokemon
+        div.append(img)
+    
+    return pokemonObject
+}
+
+const getPokemonImage = async () => {
+    const pokemon = await getRandomPokemon()
+    renderPokemon(pokemon)
+}
+
+getPokemonImage()
